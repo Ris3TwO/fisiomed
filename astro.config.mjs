733420ab9902cfb2f9f5f15529b5f566fa6d677f
Ris3TwO <manuelron.dev@gmail.com>
2025-07-09 17:42:52 -0400

    Initial improvements to the structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://example.com",
+  output: "static",
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+});
