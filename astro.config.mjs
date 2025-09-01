649f8af1586e0a0d428e0d44dbf92b0381de3b17
Ris3TwO <manuelron.dev@gmail.com>
2025-09-01 12:16:00 -0400

    dev: update imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
+import vercelServerless from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   output: "static",
-  adapter: vercel({}),
+  adapter: vercelServerless(),
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
